fc187b7425cc41704d8839107420d62275bf4d2e
Mark Brocato <mark.brocato@moovweb.com>
2020-02-06 13:45:34 +0200

    simplify router

--- routes.js
+++ routes.js
@@ -2,16 +2,10 @@ const Router = require('xdn-router/Router')
 const createNextPlugin = require('xdn-next/router/createNextPlugin')
 
 module.exports = app => {
-  const { renderNext, nextMiddleware } = createNextPlugin(app)
+  const { nextMiddleware } = createNextPlugin(app)
 
   return new Router()
     .match('/service-worker.js', ({ serveStatic }) => serveStatic('.next/service-worker.js'))
-    .match('/foo/:p', ({ render }) => {
-      render((req, res, params) => renderNext(req, res, { productId: params.p }, '/index.js'))
-    })
-    .match('/respond', ({ respond }) => {
-      respond('<html><body><h1>Static Response from Router with status 201</h1></body></html>', 201)
-    })
     .use(nextMiddleware)
     .fallback(({ proxy }) => {
       proxy('legacy', { path: '{path}' })
