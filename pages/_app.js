e8938b03b76d522582f688931a16de3be12470a6
Richard van der Dys <richard.vanderdys@gmail.com>
2020-02-13 20:18:57 +0200

    Without page component

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,52 @@
 import React from 'react'
+import theme from '../components/theme'
+import Header from '../components/Header'
+import { CssBaseline } from '@material-ui/core'
+import { makeStyles, MuiThemeProvider } from '@material-ui/core/styles'
+import PWA from 'react-storefront/PWA'
+import NavBar from '../components/NavBar'
+import reportError from '../components/reportError'
+import useJssStyles from 'react-storefront/hooks/useJssStyles'
+import installAmpOverrides from 'react-storefront-amp/installAmpOverrides'
+import Analytics from '../components/Analytics'
+import SessionProvider from 'react-storefront/session/SessionProvider'
+import AmpProvider from 'react-storefront-amp/AmpProvider'
+import useAppStore from 'react-storefront/hooks/useAppStore'
 import 'typeface-roboto'
 
-export default function MyApp({}) {
-  return <div>Empty App</div>
+installAmpOverrides()
+
+const styles = theme => ({
+  main: {
+    paddingTop: 3,
+  },
+})
+
+const useStyles = makeStyles(styles)
+
+export default function MyApp({ pageProps }) {
+  useJssStyles()
+  const classes = useStyles()
+  const [appData] = useAppStore(pageProps || {})
+
+  return (
+    <PWA errorReporter={reportError}>
+      <AmpProvider>
+        <SessionProvider url="/api/session">
+          <MuiThemeProvider theme={theme}>
+            <Analytics>
+              <CssBaseline />
+              <Header menu={appData && appData.menu} />
+              <NavBar tabs={appData && appData.tabs} />
+              <main className={classes.main}>
+                <h1>Without Page Component</h1>
+              </main>
+            </Analytics>
+          </MuiThemeProvider>
+        </SessionProvider>
+      </AmpProvider>
+    </PWA>
+  )
 }
 
 MyApp.getInitialProps = async function({ Component, ctx }) {
