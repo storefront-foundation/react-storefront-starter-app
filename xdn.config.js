fb18ee9598c74440a242c0c8858715e2f8e25481
Richard van der Dys <richard.vanderdys@gmail.com>
2020-02-21 15:49:11 +0200

    Added dev env setup
    The xdn config now pulls it's values from the .env file

--- xdn.config.js
+++ xdn.config.js
@@ -1,8 +1,22 @@
+require('dotenv').config()
+
+const { LEGACY_BACKEND_DOMAIN, LEGACY_BACKEND_HOST_HEADER } = process.env
+
+if (!LEGACY_BACKEND_DOMAIN) {
+  console.error('Please add a LEGACY_BACKEND_DOMAIN to the .env file')
+  process.exit(1)
+}
+
+if (!LEGACY_BACKEND_HOST_HEADER) {
+  console.error('Please add a LEGACY_BACKEND_HOST_HEADER to the .env file')
+  process.exit(1)
+}
+
 module.exports = {
   backends: {
     legacy: {
-      domainOrIp: 'www.ebay.com',
-      hostHeader: 'www.ebay.com',
+      domainOrIp: LEGACY_BACKEND_DOMAIN,
+      hostHeader: LEGACY_BACKEND_HOST_HEADER,
     },
   },
 }
