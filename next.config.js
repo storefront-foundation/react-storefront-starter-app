af28830616042c905a67835c9a225a2777a6176b
Richard van der Dys <richard.vanderdys@gmail.com>
2020-02-26 16:35:26 +0200

    Added single chunk optimization

--- next.config.js
+++ next.config.js
@@ -1,5 +1,14 @@
+const webpack = require('webpack')
 const withReactStorefront = require('react-storefront/plugins/withReactStorefront')
 
 module.exports = withReactStorefront({
   target: 'serverless',
+  webpack: config => {
+    config.plugins.push(
+      new webpack.optimize.LimitChunkCountPlugin({
+        maxChunks: 1,
+      })
+    )
+    return config
+  },
 })
