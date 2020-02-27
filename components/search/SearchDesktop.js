1bada8359fddefa5f4991e58d8af11c828506a99
Mark Brocato <mark.brocato@moovweb.com>
2020-02-27 15:38:41 +0200

    Only use preact if preact env var is true, some styling adjustments

--- components/search/SearchDesktop.js
+++ components/search/SearchDesktop.js
@@ -1,30 +1,41 @@
 import React, { memo, useState, useRef } from 'react'
-import { makeStyles } from '@material-ui/core/styles'
+import { makeStyles, fade } from '@material-ui/core/styles'
 import { Popover } from '@material-ui/core'
 import SearchForm from 'react-storefront/search/SearchForm'
 import SearchField from 'react-storefront/search/SearchField'
 import SearchSuggestions from 'react-storefront/search/SearchSuggestions'
 import SearchProvider from 'react-storefront/search/SearchProvider'
 
-export const styles = theme => ({
-  /**
-   * Styles applied to the root element.
-   */
-  root: {
-    display: 'flex',
-  },
-  searchinput: {
-    border: '1px solid',
-    borderRadius: theme.spacing(1),
-    margin: theme.spacing(0.5, 0, 0.5, 0),
-    zIndex: 99999,
-  },
-  paper: {
-    boxShadow: '0px 5px 5px -3px rgba(0,0,0,0.2), 0px 8px 10px 1px rgba(0,0,0,0.14)',
-    minWidth: theme.spacing(84),
-    minHeight: theme.spacing(75),
-  },
-})
+export const styles = theme => {
+  return {
+    /**
+     * Styles applied to the root element.
+     */
+    root: {
+      display: 'flex',
+    },
+    searchinput: {
+      border: '1px solid',
+      borderColor: theme.palette.divider,
+      borderRadius: theme.spacing(1),
+      margin: theme.spacing(0.5, 0, 0.5, 0),
+      zIndex: 99999,
+      transition: 'border-color linear 0.1s',
+      '&:hover': {
+        borderColor: fade(theme.palette.divider, 0.25),
+      },
+      '&:focus': {
+        borderColor: theme.palette.primary.main,
+      },
+    },
+    paper: {
+      boxShadow: '0px 5px 5px -3px rgba(0,0,0,0.2), 0px 8px 10px 1px rgba(0,0,0,0.14)',
+      minWidth: theme.spacing(84),
+      minHeight: theme.spacing(75),
+      border: `1px solid ${theme.palette.divider}`,
+    },
+  }
+}
 
 const useStyles = makeStyles(styles, { name: 'RSFSearchDesktop' })
 
@@ -46,11 +57,13 @@ function SearchDesktop({ classes }) {
         <SearchForm>
           <SearchField
             onFocus={() => {
+              // We need this so that we reopen the popup when the user comes back to it
+              // after closing and there is results in it.
               if (isFetchedRef.current) {
                 setOpen(true)
               }
             }}
-            fetchOnFirstFocus
+            // fetchOnFirstFocus
             submitButtonVariant="none"
             showClearButton={false}
             classes={{ input: classes.searchinput }}
