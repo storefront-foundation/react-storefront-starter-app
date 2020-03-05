fade5776541429093efcf176101b4bf5cb6840dc
Richard van der Dys <richard.vanderdys@gmail.com>
2020-03-05 21:44:19 +0200

    Added custom hydration with removal of stylesheet

--- components/LazyHydrate.js
+++ components/LazyHydrate.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import LazyHydrate from 'react-lazy-hydration'
 
 import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles'
 import { SheetsRegistry } from 'jss'
@@ -33,9 +32,62 @@ function LazyStylesProvider({ children }) {
   )
 }
 
+const isBrowser =
+  typeof window !== 'undefined' &&
+  typeof window.document !== 'undefined' &&
+  typeof window.document.createElement !== 'undefined'
+
+function LazyHydrate({ hydrated, children, on, index, ...rest }) {
+  const childRef = React.useRef(null)
+  const [_hydrated, setHydrated] = React.useState(!isBrowser)
+
+  if (on === 'click') {
+    React.useEffect(() => {
+      function hydrate() {
+        setHydrated(true)
+      }
+      childRef.current.addEventListener('click', hydrate, {
+        once: true,
+        capture: true,
+        passive: true,
+      })
+      return () => {
+        childRef.current.removeEventListener('click', hydrate, { capture: true })
+      }
+    })
+  }
+
+  React.useEffect(() => {
+    if (hydrated || _hydrated) {
+      const stylesheet = window.document.getElementById(`jss-lazy-${index}`)
+      if (stylesheet) {
+        stylesheet.remove()
+      }
+    }
+  })
+
+  if (hydrated || _hydrated) {
+    return (
+      <div ref={childRef} style={{ display: 'contents' }} {...rest}>
+        {children}
+      </div>
+    )
+  } else {
+    return (
+      <div
+        ref={childRef}
+        style={{ display: 'contents' }}
+        suppressHydrationWarning
+        {...rest}
+        dangerouslySetInnerHTML={{ __html: '' }}
+      />
+    )
+  }
+}
+
 export default function({ children, ...props }) {
   return (
-    <LazyHydrate {...props}>
+    <LazyHydrate {...props} index={registries.length}>
       <LazyStylesProvider>{children}</LazyStylesProvider>
     </LazyHydrate>
   )
