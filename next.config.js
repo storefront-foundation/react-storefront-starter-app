3b1410239f8f52fd297c48372d0893f984f86983
Richard van der Dys <richard.vanderdys@gmail.com>
2020-03-09 17:28:22 +0200

    Putting back 1 chunk limit

--- next.config.js
+++ next.config.js
@@ -1,14 +1,14 @@
-// const webpack = require('webpack')
+const webpack = require('webpack')
 const withReactStorefront = require('react-storefront/plugins/withReactStorefront')
 
 module.exports = withReactStorefront({
   target: 'serverless',
-  // webpack: config => {
-  // config.plugins.push(
-  //   new webpack.optimize.LimitChunkCountPlugin({
-  //     maxChunks: 1,
-  //   })
-  // )
-  //   return config
-  // },
+  webpack: config => {
+    config.plugins.push(
+      new webpack.optimize.LimitChunkCountPlugin({
+        maxChunks: 1,
+      })
+    )
+    return config
+  },
 })
