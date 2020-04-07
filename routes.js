86c2d66aaede849e327c0093d5214b9ed041b7ef
Mark Brocato <mark.brocato@moovweb.com>
2020-04-07 09:42:53 +0300

    Add color and size to whitelisted query parameters.

--- routes.js
+++ routes.js
@@ -1,21 +1,18 @@
 const { Router } = require('@xdn/core/router')
 const { createNextPlugin } = require('@xdn/next')
 const { API, SSR, SERVICE_WORKER, cacheResponse } = require('./cache')
+const { nextMiddleware, renderNext } = createNextPlugin()
 
-module.exports = app => {
-  const { nextMiddleware, renderNext } = createNextPlugin(app)
-
-  return new Router()
-    .match('/service-worker.js', async ({ cache, serveStatic }) => {
-      cache(SERVICE_WORKER)
-      await serveStatic('.next/static/service-worker.js')
-    })
-    .match('/', cacheResponse(SSR))
-    .match('/api/', cacheResponse(API))
-    .match('/s/:subcategoryId', cacheResponse(SSR))
-    .match('/api/s/:subcategoryId', cacheResponse(API))
-    .match('/p/:productId', cacheResponse(SSR))
-    .match('/api/p/:productId', cacheResponse(API))
-    .use(nextMiddleware)
-    .fallback(({ proxy }) => proxy('legacy'))
-}
+module.exports = new Router()
+  .match('/service-worker.js', async ({ cache, serveStatic }) => {
+    cache(SERVICE_WORKER)
+    await serveStatic('.next/static/service-worker.js')
+  })
+  .match('/', cacheResponse(SSR))
+  .match('/api/', cacheResponse(API))
+  .match('/s/:subcategoryId', cacheResponse(SSR))
+  .match('/api/s/:subcategoryId', cacheResponse(API))
+  .match('/p/:productId', cacheResponse(SSR))
+  .match('/api/p/:productId', cacheResponse(API))
+  .use(nextMiddleware)
+  .fallback(({ proxy }) => proxy('legacy'))
