edff8a6dd96575d19785be968b2b26f1d04b426f
Kevin Henderson <kevhender@gmail.com>
2020-04-08 14:37:42 -0400

    Update for carousel changes - Add varying image widths for PDP, and lightbox CSS class (#44)

--- components/mocks/createMedia.js
+++ components/mocks/createMedia.js
@@ -2,23 +2,23 @@ import colors from './colors'
 
 export default function createMedia(id, color) {
   return {
-    full: [color].map(key => ({
-      src: `https://via.placeholder.com/600x600/${colors[key].background}/${
-        colors[key].foreground
-      }?text=${encodeURIComponent('Product ' + id)}`,
+    full: [color].map((key, i) => ({
+      src: `https://via.placeholder.com/${i === 2 ? 400 : 600}x${i === 1 ? 400 : 600}/${
+        colors[key].background
+      }/${colors[key].foreground}?text=${encodeURIComponent('Product ' + id)}`,
       alt: `Product ${id}`,
       magnify: {
-        height: 1200,
-        width: 1200,
-        src: `https://via.placeholder.com/1200x1200/${colors[key].background}/${
-          colors[key].foreground
-        }?text=${encodeURIComponent('Product ' + id)}`,
+        height: i === 1 ? 800 : 1200,
+        width: i === 2 ? 800 : 1200,
+        src: `https://via.placeholder.com/${i === 2 ? 800 : 1200}x${i === 1 ? 800 : 1200}/${
+          colors[key].background
+        }/${colors[key].foreground}?text=${encodeURIComponent('Product ' + id)}`,
       },
     })),
-    thumbnails: [color].map(key => ({
-      src: `https://via.placeholder.com/400x400/${colors[key].background}?text=${encodeURIComponent(
-        `Product ${id}`
-      )}`,
+    thumbnails: [color].map((key, i) => ({
+      src: `https://via.placeholder.com/${i === 2 ? 300 : 400}x${i === 1 ? 300 : 400}/${
+        colors[key].background
+      }?text=${encodeURIComponent(`Product ${id}`)}`,
       alt: key,
     })),
   }
