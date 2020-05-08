1dd2ab27009f139e360e9555cf9fc14c53afb20b
Mark Brocato <mark.brocato@moovweb.com>
2020-05-08 20:35:58 +0300

    use @xdn/prefetch (#51)
    * use @xdn/prefetch
    
    * update xdn

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
 const webpack = require('webpack')
 const withReactStorefront = require('react-storefront/plugins/withReactStorefront')
+const { PREFETCH_QUERY_PARAM } = require('@xdn/prefetch/constants')
 
 module.exports = withReactStorefront({
   target: 'serverless',
+  prefetchQueryParam: PREFETCH_QUERY_PARAM,
   webpack: config => {
     config.plugins.push(
       new webpack.optimize.LimitChunkCountPlugin({
