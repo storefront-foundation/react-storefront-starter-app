7f140393edd80294f2d4d973bbfd33eadbedd05c
Mark Brocato <mark.brocato@moovweb.com>
2020-06-18 14:36:22 +0300

    Add withXDN

--- next.config.js
+++ next.config.js
@@ -1,17 +1,20 @@
 const webpack = require('webpack')
 const withReactStorefront = require('react-storefront/plugins/withReactStorefront')
 const { PREFETCH_QUERY_PARAM } = require('@xdn/prefetch/constants')
+const withXDN = require('@xdn/next/withXDN')
 
-module.exports = withReactStorefront({
-  target: 'serverless',
-  prefetchQueryParam: PREFETCH_QUERY_PARAM,
-  connector: 'react-storefront/mock-connector',
-  webpack: config => {
-    config.plugins.push(
-      new webpack.optimize.LimitChunkCountPlugin({
-        maxChunks: 1,
-      })
-    )
-    return config
-  },
-})
+module.exports = withXDN(
+  withReactStorefront({
+    target: 'serverless',
+    prefetchQueryParam: PREFETCH_QUERY_PARAM,
+    connector: 'react-storefront/mock-connector',
+    webpack: config => {
+      config.plugins.push(
+        new webpack.optimize.LimitChunkCountPlugin({
+          maxChunks: 1,
+        })
+      )
+      return config
+    },
+  })
+)
