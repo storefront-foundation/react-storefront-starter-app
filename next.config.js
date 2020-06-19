168ee8b9d225c01c20f1808bc1ed4006437bd951
Mark Brocato <mark.brocato@moovweb.com>
2020-06-19 15:03:22 +0300

    Fix conflicts.

--- next.config.js
+++ next.config.js
@@ -1,21 +1,22 @@
 const webpack = require('webpack')
 const withReactStorefront = require('react-storefront/plugins/withReactStorefront')
 const { PREFETCH_QUERY_PARAM } = require('@xdn/prefetch/constants')
-// const withXDN = require('@xdn/next/withXDN')
+const withXDN = require('@xdn/next/withXDN')
 
-module.exports /*withXDN(*/ = withReactStorefront({
-  target: 'serverless',
-  prefetchQueryParam: PREFETCH_QUERY_PARAM,
-  connector: 'react-storefront/mock-connector',
-  webpack: (config, options) => {
-    if (!options.isServer) {
-      config.plugins.push(
-        new webpack.optimize.LimitChunkCountPlugin({
-          maxChunks: 1,
-        })
-      )
-    }
-    return config
-  },
-})
-// )
+module.exports = withXDN(
+  withReactStorefront({
+    target: 'serverless',
+    prefetchQueryParam: PREFETCH_QUERY_PARAM,
+    connector: 'react-storefront/mock-connector',
+    webpack: (config, options) => {
+      if (!options.isServer) {
+        config.plugins.push(
+          new webpack.optimize.LimitChunkCountPlugin({
+            maxChunks: 1,
+          })
+        )
+      }
+      return config
+    },
+  })
+)
