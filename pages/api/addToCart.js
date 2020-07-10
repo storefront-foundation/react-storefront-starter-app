f5543f53ba08f72cd4c578319db588b4952768f4
Egor Mesyats <egor.mesyats@moovweb.com>
2020-07-10 20:40:05 +0300

    use `addToCart` instead of POST `cart`

--- pages/api/addToCart.js
+++ pages/api/addToCart.js
@@ -1,6 +1,5 @@
-import { cart } from 'react-storefront-connector'
+import { addToCart } from 'react-storefront-connector'
 
 export default function(req, res) {
-  // using connector's `cart` POST as Add To Cart
-  return cart(req, res)
+  return addToCart(req, res)
 }
