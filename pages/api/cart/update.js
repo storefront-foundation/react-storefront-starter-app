9e0eb6b7be127dc46af54378dbd544d71577f0a4
Mark Brocato <mark.brocato@moovweb.com>
2020-07-16 23:36:24 +0300

    updateCart => updateCartItem

--- pages/api/cart/update.js
+++ pages/api/cart/update.js
@@ -2,7 +2,7 @@ import { updateCart } from 'react-storefront-connector'
 
 export async function handler(req, res) {
   const { item, quantity } = req.body
-  res.json(await updateCart(item, quantity, req, res))
+  res.json(await updateCartItem(item, quantity, req, res))
 }
 
 export const config = {
