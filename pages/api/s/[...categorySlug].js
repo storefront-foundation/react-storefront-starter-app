cbf2dd3d7508b21c0b1d642b358ff1b0ebe2d41f
Egor Mesyats <egor.mesyats@moovweb.com>
2020-07-22 16:16:15 +0300

    PLP: catch unexpected errors

--- pages/api/s/[...categorySlug].js
+++ pages/api/s/[...categorySlug].js
@@ -2,5 +2,20 @@ import { subcategory } from 'react-storefront-connector'
 
 export default async function plp(req, res) {
   const { q, categorySlug: slug, page, filters, sort } = req.query
-  res.json(await subcategory({ q, slug, page, filters, sort }, req, res))
+  try {
+    const data = await subcategory({ q, slug, page, filters, sort }, req, res)
+    return res.json(data)
+  } catch (error) {
+    console.error(error)
+    return res.json({
+      pageData: {
+        id: 'error',
+        name: 'Unexpected error',
+        title: 'Unexpected error',
+        products: [],
+        total: 0,
+        page: 1,
+      },
+    })
+  }
 }
