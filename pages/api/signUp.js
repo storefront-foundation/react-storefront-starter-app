513acfdac582520c074a8cd76227dc24f1dab71d
Richard van der Dys <richard.vanderdys@gmail.com>
2020-08-18 19:35:53 +0300

    Changes to support connector interface (#69)
    * getting salesforce connect4ed
    
    * Needed changes
    
    * Updating conenctor
    
    * Needs for salesforce
    
    * lock changes
    
    * reverting
    
    * Apply suggestions from code review
    
    Co-authored-by: Mark Brocato <mark.brocato@moovweb.com>

--- pages/api/signUp.js
+++ pages/api/signUp.js
@@ -1,11 +1,12 @@
 import { signUp } from 'react-storefront-connector'
 
 export default function(req, res) {
-  return signUp(req.body, req, res)
+  const data = typeof req.body === 'string' ? JSON.parse(req.body) : req.body
+  return signUp(data, req, res)
 }
 
 export const config = {
   api: {
-    bodyParser: false,
+    bodyParser: true,
   },
 }
