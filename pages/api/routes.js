25d17f29c371290a1db8e2962496553c369e6fd7
Mark Brocato <mark.brocato@moovweb.com>
2020-09-28 15:55:41 +0300

    Cache routes

--- pages/api/routes.js
+++ pages/api/routes.js
@@ -1,5 +1,6 @@
 import listRoutes from 'react-storefront/server/listRoutes'
 
 export default async function routes(req, res) {
+  res.setHeader('cache-control', 'max-age: ' + 60 * 60 * 24 * 365)
   res.json(listRoutes())
 }
