d0427ab6c0ed950ccf186e9a93542055af318a84
Mark Brocato <mark.brocato@moovweb.com>
2021-04-14 13:00:10 +0300

    Use default rum routing.

--- components/rum.js
+++ components/rum.js
@@ -1,12 +1,5 @@
 import { Metrics } from '@layer0/rum'
-import Router from '@layer0/rum/Router'
 
 if (typeof window !== 'undefined') {
-  new Metrics({
-    debug: true,
-    router: new Router()
-      .match('/p/:id', ({ setPageLabel }) => setPageLabel('product'))
-      .match('/s/:id', ({ setPageLabel }) => setPageLabel('subcategory'))
-      .match('/', ({ setPageLabel }) => setPageLabel('home')),
-  }).collect()
+  new Metrics().collect()
 }
