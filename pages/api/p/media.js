4a260d54a3f0cf6cf8b105b9ecadaa70f2830ee0
artsiomyemelyanenka <Artsiom_Yemelyanenka@epam.com>
2021-04-14 06:57:24 -0400

    fix build issues (#89)

--- pages/api/p/media.js
+++ pages/api/p/media.js
@@ -1,4 +1,4 @@
-export { productMedia as default } from 'react-storefront-connector'
+export { productMedia } from 'react-storefront-connector'
 
 export default async function(req, res) {
   const { productId, color } = req.query
