e150be8e791d10e2b59431346fff6b74b472075c
Divyakumar <divyakumarjain@gmail.com>
2021-06-11 00:05:15 +0530

    Working storybook and upgraded dependencies

---
+++ .storybook/stories/Accordion.stories.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { ExpandableSection } from 'react-storefront'
+import { Accordion } from 'react-storefront'
+
+export default { title: 'Accordion' }
+
+export const defaults = () => (
+  <Accordion>
+    <ExpandableSection title="First">
+      <div>The first section</div>
+    </ExpandableSection>
+    <ExpandableSection title="Second">
+      <div>The second section</div>
+    </ExpandableSection>
+    <ExpandableSection title="Third">
+      <div>The third section</div>
+    </ExpandableSection>
+  </Accordion>
+)
---
+++ .storybook/stories/ProductOptionSelector.stories.js
@@ -0,0 +1,61 @@
+import React, { useState } from 'react'
+import { ProductOptionSelector } from 'react-storefront/option'
+import { SwatchProductOption } from 'react-storefront/option'
+
+export default { title: 'ProductOptionSelector' }
+
+const colors = [
+  {
+    text: 'Red',
+    id: 'red',
+    image: {
+      src: 'https://via.placeholder.com/48x48/f44336?text=%20',
+      alt: 'red',
+    },
+  },
+  {
+    text: 'Green',
+    id: 'green',
+    image: {
+      src: 'https://via.placeholder.com/48x48/4caf50?text=%20',
+      alt: 'green',
+    },
+  },
+  {
+    text: 'Blue',
+    id: 'blue',
+    image: {
+      src: 'https://via.placeholder.com/48x48/2196f3?text=%20',
+      alt: 'blue',
+    },
+  },
+  {
+    text: 'Grey',
+    id: 'grey',
+    image: {
+      src: 'https://via.placeholder.com/48x48/e0e0e0?text=%20',
+      alt: 'grey',
+    },
+  },
+]
+
+export const swatch = () => {
+  const [color, setColor] = useState(colors[0])
+
+  return (
+    <ProductOptionSelector options={colors} value={color} onChange={color => setColor(color)} />
+  )
+}
+
+export const text = () => {
+  const [color, setColor] = useState(colors[0])
+
+  return (
+    <ProductOptionSelector
+      options={colors}
+      value={color}
+      onChange={color => setColor(color)}
+      variant="text"
+    />
+  )
+}
