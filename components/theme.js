ad7c387c68ba67cefac59eb26d92659ac2df3116
Christopher Buenaventura <christopherbuenaventura@gmail.com>
2021-12-02 14:39:40 -0800

    Remove adaptV4Theme usage

--- components/theme.js
+++ components/theme.js
@@ -1,25 +1,22 @@
 import createTheme from 'react-storefront/theme/createTheme'
 import { red } from '@mui/material/colors'
-import { adaptV4Theme } from '@mui/material/styles'
 
-const theme = createTheme(
-  adaptV4Theme({
-    palette: {
-      primary: {
-        main: '#556cd6',
-      },
-      secondary: {
-        main: '#19857b',
-      },
-      error: {
-        main: red.A400,
-      },
-      background: {
-        default: '#fff',
-      },
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#556cd6',
     },
-    overrides: {},
-  })
-)
+    secondary: {
+      main: '#19857b',
+    },
+    error: {
+      main: red.A400,
+    },
+    background: {
+      default: '#fff',
+    },
+  },
+  components: {},
+})
 
 export default theme
