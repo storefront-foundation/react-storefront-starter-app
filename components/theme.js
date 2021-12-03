4be3b2d942b38fece7ceb41beb5c01bb6666d689
sudo97 <illia.mikhnevych@moovforge.com>
2021-12-03 17:36:25 +0200

    theme config

--- components/theme.js
+++ components/theme.js
@@ -1,22 +1,25 @@
 import createTheme from 'react-storefront/theme/createTheme'
+import { adaptV4Theme } from '@mui/material/styles'
 import { red } from '@mui/material/colors'
 
-const theme = createTheme({
-  palette: {
-    primary: {
-      main: '#556cd6',
+const theme = createTheme(
+  adaptV4Theme({
+    palette: {
+      primary: {
+        main: '#556cd6',
+      },
+      secondary: {
+        main: '#19857b',
+      },
+      error: {
+        main: red.A400,
+      },
+      background: {
+        default: '#fff',
+      },
     },
-    secondary: {
-      main: '#19857b',
-    },
-    error: {
-      main: red.A400,
-    },
-    background: {
-      default: '#fff',
-    },
-  },
-  components: {},
-})
+    components: {},
+  })
+)
 
 export default theme
